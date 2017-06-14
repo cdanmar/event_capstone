9d360bda460c30a766d9a501b96beee1fa6de4b5
cdanmar <cdanmar@gmail.com>
2017-06-14 18:02:19 -0500

    June 14 work

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -12,7 +12,7 @@ class AdminsController < ApplicationController
       redirect_to '/signup'
     end
   end
-
+ 
 private
   def admin_params
     params.require(:admin).permit(:name, :email, :password, :password_confirmation)
